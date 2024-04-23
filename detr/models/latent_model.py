c534eec510f9392eb9419705f984f87b7b450d20
lukezihe <lukezihe@gmail.com>
2024-04-23 14:51:07 -0700

    black format

--- detr/models/latent_model.py
+++ detr/models/latent_model.py
@@ -4,6 +4,7 @@ import torch
 
 DROPOUT_RATE = 0.1
 
+
 # a causal transformer block
 class Causal_Transformer_Block(nn.Module):
     def __init__(self, seq_len, latent_dim, num_head) -> None:
@@ -11,7 +12,9 @@ class Causal_Transformer_Block(nn.Module):
         self.num_head = num_head
         self.latent_dim = latent_dim
         self.ln_1 = nn.LayerNorm(latent_dim)
-        self.attn = nn.MultiheadAttention(latent_dim, num_head, dropout=DROPOUT_RATE, batch_first=True)
+        self.attn = nn.MultiheadAttention(
+            latent_dim, num_head, dropout=DROPOUT_RATE, batch_first=True
+        )
         self.ln_2 = nn.LayerNorm(latent_dim)
         self.mlp = nn.Sequential(
             nn.Linear(latent_dim, 4 * latent_dim),
@@ -21,19 +24,25 @@ class Causal_Transformer_Block(nn.Module):
         )
 
         # self.register_buffer("attn_mask", torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool())
-    
+
     def forward(self, x):
-        attn_mask = torch.triu(torch.ones(x.shape[1], x.shape[1], device=x.device, dtype=torch.bool), diagonal=1)
+        attn_mask = torch.triu(
+            torch.ones(x.shape[1], x.shape[1], device=x.device, dtype=torch.bool),
+            diagonal=1,
+        )
         x = self.ln_1(x)
         x = x + self.attn(x, x, x, attn_mask=attn_mask)[0]
         x = self.ln_2(x)
         x = x + self.mlp(x)
-        
+
         return x
+
 
 # use self-attention instead of RNN to model the latent space sequence
 class Latent_Model_Transformer(nn.Module):
-    def __init__(self, input_dim, output_dim, seq_len, latent_dim=256, num_head=8, num_layer=3) -> None:
+    def __init__(
+        self, input_dim, output_dim, seq_len, latent_dim=256, num_head=8, num_layer=3
+    ) -> None:
         super().__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
@@ -45,11 +54,14 @@ class Latent_Model_Transformer(nn.Module):
         self.weight_pos_embed = nn.Embedding(seq_len, latent_dim)
         self.attention_blocks = nn.Sequential(
             nn.Dropout(DROPOUT_RATE),
-            *[Causal_Transformer_Block(seq_len, latent_dim, num_head) for _ in range(num_layer)],
+            *[
+                Causal_Transformer_Block(seq_len, latent_dim, num_head)
+                for _ in range(num_layer)
+            ],
             nn.LayerNorm(latent_dim)
         )
         self.output_layer = nn.Linear(latent_dim, output_dim)
-    
+
     def forward(self, x):
         x = self.input_layer(x)
         x = x + self.weight_pos_embed(torch.arange(x.shape[1], device=x.device))
@@ -57,17 +69,20 @@ class Latent_Model_Transformer(nn.Module):
         logits = self.output_layer(x)
 
         return logits
-    
+
     @torch.no_grad()
     def generate(self, n, temperature=0.1, x=None):
         if x is None:
-            x = torch.zeros((n, 1, self.input_dim), device=self.weight_pos_embed.weight.device)
+            x = torch.zeros(
+                (n, 1, self.input_dim), device=self.weight_pos_embed.weight.device
+            )
         for i in range(self.seq_len):
             logits = self.forward(x)[:, -1]
             probs = torch.softmax(logits / temperature, dim=-1)
             samples = torch.multinomial(probs, num_samples=1)[..., 0]
-            samples_one_hot = F.one_hot(samples.long(), num_classes=self.output_dim).float()
+            samples_one_hot = F.one_hot(
+                samples.long(), num_classes=self.output_dim
+            ).float()
             x = torch.cat([x, samples_one_hot[:, None, :]], dim=1)
-        
-        return x[:, 1:, :]
 
+        return x[:, 1:, :]
